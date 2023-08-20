908a8af9bfc3e4da70f256eb7327b848d03a301c
Mathias Biilmann Christensen <info@mathias-biilmann.net>
2023-08-20 22:10:54 +0200

    Try cache off for edge function

--- netlify/edge-functions/esi.ts
+++ netlify/edge-functions/esi.ts
@@ -77,4 +77,4 @@ export default async (request: Request, context: Context) => {
     return new Response(bodyStream, response);
 }
 
-export const config: Config = { path: "/*", cache: "manual" };
+export const config: Config = { path: "/*" };
